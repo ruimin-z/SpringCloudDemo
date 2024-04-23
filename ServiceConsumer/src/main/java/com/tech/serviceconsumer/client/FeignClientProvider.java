c3c7b6ec58321f1a85679590f6b10d06dbf1a8cb
Ruimin Zhang <ruiminz1@gmail.com>
2024-04-22 18:28:06 -0700

    spring cloud + Consul

--- ServiceConsumer/src/main/java/com/tech/serviceconsumer/client/FeignClientProvider.java
+++ ServiceConsumer/src/main/java/com/tech/serviceconsumer/client/FeignClientProvider.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="ServiceProvider")  // 长连接，name表明调用service provider1的API是下面的样子
+@FeignClient(name="ServiceProvider")  // 要和consul里的名字一样，和consul协作 - 服务发现
+// 长连接，name表明调用service provider1的API是下面的样子。通过RPC调用代码时，可以用什么接口
+// 接口要和HelloController里一样
 public interface FeignClientProvider {
     /**
      * Define ServiceProvider Interface
